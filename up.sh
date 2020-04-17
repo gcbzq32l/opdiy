9c1f53b2ad0d0c6543526c8c6cb28719d0f5e084
jsda <jsda@github.com>
2020-04-18 05:11:00 +0800

    20200418-0511

--- up.sh
+++ up.sh
@@ -5,7 +5,7 @@ git clone --depth 1 https://github.com/pexcn/openwrt-chinadns-ng.git ../chinadns
 git clone --depth 1 https://github.com/kuoruan/luci-app-v2ray.git ../luci-app-v2ray && [ -e ../luci-app-v2ray/Makefile ] && rm -rf ../luci-app-v2ray/.git && rm -rf diy/luci-app-v2ray && cp -a ../luci-app-v2ray diy && echo "luci-app-v2ray更新成功" || echo "luci-app-v2ray暂无更新"
 git clone --depth 1 https://github.com/rufengsuixing/luci-app-adguardhome.git ../luci-app-adguardhome && [ -e ../luci-app-adguardhome/Makefile ] && rm -rf ../luci-app-adguardhome/.git && rm -rf diy/luci-app-adguardhome && cp -a ../luci-app-adguardhome diy && echo "luci-app-adguardhome更新成功" || echo "luci-app-adguardhome暂无更新"
 #更新AdGuardHome.yaml
-wget https://github.com/felixonmars/dnsmasq-china-list/raw/master/accelerated-domains.china.conf && \
+wget -q https://github.com/felixonmars/dnsmasq-china-list/raw/master/accelerated-domains.china.conf && \
 sed -e 's|^server=/\(.*\)/114.114.114.114$$|\1|' accelerated-domains.china.conf | egrep -v '^#' > accelerated-domains.china.raw.txt && \
 sed -i "s/server=/  - '[/g;s/114.114.114.114/]https:\/\/dns.alidns.com\/dns-query'/g" accelerated-domains.china.raw.txt && \
 sed -i '/dns.alidns.com\/dns-query/d' config/AdGuardHome/AdGuardHome.yaml && \
